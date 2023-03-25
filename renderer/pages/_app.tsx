e0103ee853e94b0d2f6aa8b424fb3f880f67bc6b
smith <pfgoasleep@foxmail.com>
2023-03-25 20:28:17 +0800

    refactor: update antd and builder

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import Head from 'next/head';
-import type { AppProps } from 'next/app';
-
-import 'antd/dist/antd.css';
+import React from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </React.Fragment>
